b48fe24b1d39ef4cbd5ac851225c7cf8bb86fdbb
BrettMyburgh <myburghb@gmail.com>
2026-02-03 22:30:03 +0200

    Added accessories to player

--- src/Player/player_base.py
+++ src/Player/player_base.py
@@ -1,6 +1,8 @@
 import pygame
 from .player_sprite import playerSprite
 from pygame.sprite import Group
+import os
+import random
 
 class player:
 
@@ -11,4 +13,29 @@ class player:
         sprite = playerSprite(screenx,screeny,"Character/" + "Char1.png")
         player = Group()
         player.add(sprite)
+        player = self.accessories(screenx, screeny, player)
         return player
+    
+    def accessories(self, x, y, player_group):
+        hair_count = 0
+        beard_count = 0
+        directory = "src/Sprites/FreeAssets/Character/Accessories/"
+        test = os.listdir(directory)
+        hair_list = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and "Hair" in f]
+        beard_list = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and "Beard" in f]
+        if random.randint(0,10) > 5:
+            beard_file = self.get_random_file(beard_list)
+            sprite = playerSprite(x,y,"Character/Accessories/" + beard_file)
+            player_group.add(sprite)
+        hair_file = self.get_random_file(hair_list)
+        hair_sprite = playerSprite(x,y,"Character/Accessories/" + hair_file)
+        player_group.add(hair_sprite)
+        return player_group
+
+            
+
+    def get_random_file(self, file_list):
+        file_name = random.choice(file_list)
+        return file_name
+
+
