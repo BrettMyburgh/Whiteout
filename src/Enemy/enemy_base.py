9cd977231200434bb2359ccd79193946eec8cc3b
BrettMyburgh <myburghb@gmail.com>
2026-02-05 22:15:06 +0200

    Added start for enemy

---
+++ src/Enemy/enemy_base.py
@@ -0,0 +1,7 @@
+import pygame
+
+class enemy_base():
+    def __init__(self):
+        pass
+
+    def load_enemies
---
+++ src/Enemy/enemy_sprite.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+from pathlib import Path
+
+class enemySprite(Sprite):
+
+    script_dir = Path(__file__).parent
+
+    def __init__(self, x, y, file_name):
+        super().__init__()
+        self.image = pygame.image.load(self.script_dir.parent.parent / file_name)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+    def get_rect(self):
+        return self.rect
+    
+    def update(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
